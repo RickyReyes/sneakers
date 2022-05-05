b002e7c89a239ec4a89197b4bea896923ec47d1b
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-05-05 15:43:07 -0700

    mobile menu shortened

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -19,21 +19,6 @@ export default function MobileMenu() {
                 <li className="mobile-menu-item">
                     <a>Contact</a>
                 </li>
-                <li className="mobile-menu-item">
-                    <a>Collections</a>
-                </li>
-                <li className="mobile-menu-item">
-                    <a>Men</a>
-                </li>
-                <li className="mobile-menu-item">
-                    <a>Women</a>
-                </li>
-                <li className="mobile-menu-item">
-                    <a>About</a>
-                </li>
-                <li className="mobile-menu-item">
-                    <a>Contact</a>
-                </li>
             </ul>
         <div className="dark-overlay">
         </div>
